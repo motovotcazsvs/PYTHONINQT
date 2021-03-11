7a33bdbf65a40051173b6d35b4d4dfc77202425c
motovotcazsvs <78682549+motovotcazsvs@users.noreply.github.com>
2021-03-11 23:15:54 +0200

    розделено на строки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //QProcess p;
+
 }
 
 MainWindow::~MainWindow()
@@ -29,19 +29,28 @@ void MainWindow::on_pushButton_clicked()
     p.waitForFinished(-1);
     QString p_stdout = QString::fromLocal8Bit(p.readAll());
     p_stdout = p_stdout.simplified();
+
     lbl.setText(p_stdout);
-    QString p_stdout2 = "vova";
-    qDebug() << p_stdout << p_stdout2;
     lbl.show();
 
-    //QString str1 = p_stdout.right(p_stdout.indexOf('Сейчас в эфире Радио Рекорд'));
-
     p_stdout.remove("ейчас в эфире Радио Рекорд с");
-    //qDebug() << str1 << p_stdout;// << endl << str2 << endl << str3;
-    QString str1 = p_stdout;
-    qDebug() << str1.left(str1.indexOf(" Плейлист Радио Рекорд"));
-    QString str2 = p_stdout;
-    qDebug() << str2.left(str2.indexOf(" Плейлист Радио Рекорд"));
+    p_stdout.remove(" Плейлист Радио Рекорд");
+    qDebug() << p_stdout;
+
+    QString str_track1 = p_stdout;
+    str_track1 = str_track1.left(str_track1.indexOf(":") - 3);
+    p_stdout.remove(str_track1);
+    p_stdout.remove(p_stdout.left(p_stdout.indexOf(":") + 4));
+
+    QString str_track3 = p_stdout;
+    str_track3.remove(str_track3.left(str_track3.indexOf(":") + 4));
+    str_track3 = str_track3.left(str_track3.indexOf(":") - 3);
+
+    QString str_track2 = p_stdout;
+    str_track2 = str_track2.left(str_track2.indexOf(":") - 3);
+
+    qDebug() << str_track1 << endl << str_track2 << endl << str_track3;
+
 }
 
 void MainWindow::on_pushButton_2_clicked()
